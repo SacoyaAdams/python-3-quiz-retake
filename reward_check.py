797ec7d8fed3abdc4222f9643b14d5194b929682
SacoyaAdams <114244564+SacoyaAdams@users.noreply.github.com>
2023-07-11 19:07:39 -0400

    test

--- reward_check.py
+++ reward_check.py
@@ -1,6 +1,16 @@
 
 def find_best_user(file_path):
-    pass #TODO
+    # pass #TODO
+    string = ""
+    with open(file_path, 'r') as file:
+        next(file) # skip the 1st row
+        for line in file:
+            info = line.strip().split(', ')
+            # if user have the highest points print their name 
+            if string == 21:
+                print(string)
+
+            
 
 
 def test():
